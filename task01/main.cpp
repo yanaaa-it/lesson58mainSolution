3b747f5ea82cc0fbeebadd749ac1286eae9e8b4f
Белая Яна <yaninalikemiki@gmail.com>
2026-05-01 11:42:17 +0300

    new

--- task01/main.cpp
+++ task01/main.cpp
@@ -2,15 +2,20 @@
 
 int main() {
 	int* a = new int();
-    int* b = new int();
+	int* b = new int();
 	int* c = new int();
+
 
 	cout << "input a,b,c:";
 	cin >> *a >> *b >> *c;
 
-	print("Befor: "+to_string(*a) + " " + to_string(*b) + " " + to_string(*c) + "\n");
+	if (a == NULL || b == NULL || c == NULL) {
+		return 1;
+	}
+
+	print("Befor: " + to_string(*a) + " " + to_string(*b) + " " + to_string(*c) + "\n");
 	swap(a, b, c);
-	print("After: "+to_string(*a) + " " + to_string(*b) + " " + to_string(*c) + "\n");
+	print("After: " + to_string(*a) + " " + to_string(*b) + " " + to_string(*c) + "\n");
 
 	remove(a);
 	remove(b);
